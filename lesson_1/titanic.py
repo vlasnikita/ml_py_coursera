db937333f7696477846ea7c86d5d9fd05d35a641
Nikita Vlasenko <vlasenkonikita@gmail.com>
2018-08-11 15:10:35 +0300

    mend

---
+++ lesson_1/titanic.py
@@ -0,0 +1,37 @@
+import pandas
+import numpy as np
+from sklearn import tree
+from sklearn.datasets import load_iris
+import graphviz
+
+PATH = '/Users/vlasnikita/Documents/vls/py/ml_py_coursera/'
+
+data = pandas.read_csv(PATH + 'titanic.csv', index_col='PassengerId')
+data = data[['Survived', 'Pclass', 'Fare', 'Age', 'Sex']]
+data = data.dropna(axis=0)
+
+X = data[['Pclass', 'Fare', 'Age', 'Sex']].copy()
+sex_mapping = {'male': 0, 'female': 1}
+X = X.replace({'Sex': sex_mapping})
+
+y = data[['Survived']]
+
+clf = tree.DecisionTreeClassifier(random_state=241)
+clf.fit(X,y)
+
+importances = np.array(clf.feature_importances_)
+
+print importances
+
+dot_data = tree.export_graphviz(
+    clf, 
+    out_file=None,
+    feature_names=['Pclass', 'Fare', 'Age', 'Sex'],
+    filled=True, 
+    rounded=True,  
+    special_characters=True,
+    class_names=True
+)
+
+graph = graphviz.Source(dot_data)
+graph.render("./ml_py_coursera/lesson_1/tree")
